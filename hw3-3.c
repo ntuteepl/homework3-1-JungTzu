2642e82115cb3a7cdea359ea5125606c1089485c
JungTzu <146064914+JungTzu@users.noreply.github.com>
2023-10-24 12:03:57 +0800

    Update hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -6,9 +6,31 @@ int main() {
 int num1,num2;
 scanf ("%d %d",&num1,&num2);
 
+int digit1 , digit2;
+int num01 = num1 , num02 = num2;
+
+if (num01 == 0) {
+      digit1 = 1;
+} else {
+    while (num01 != 0) {
+        num01 /= 10;
+          digit1++;
+    }
+}
+
+if (num02 == 0) {
+      digit2 = 1;
+} else {
+    while (num02 != 0) {
+        num02 /= 10;
+          digit2++;
+    }
+}
+/*
 //判斷num1、num2位數
   int digit1 = (int)log10(num1) + 1;
   int digit2 = (int)log10(num2) + 1;
+*/
 
 int total1,total2;
 int count1 = num1 , count2 = num2;
